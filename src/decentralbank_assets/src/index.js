16f651d23bf807335520320f1857c3f994b0a6e2
Aniket Kini <47451732+anstormx@users.noreply.github.com>
2024-06-03 22:34:59 +0530

    commit

--- src/decentralbank_assets/src/index.js
+++ src/decentralbank_assets/src/index.js
@@ -2,13 +2,13 @@ import {
   decentralbank
 } from "../../declarations/decentralbank";
 
-window.addEventListener('load', async function() {  //will get executed after the page is loaded
+window.addEventListener('load', async function () { //will get executed after the page is loaded
   const balance = await decentralbank.checkbalance();
-  document.getElementById("value").innerText = Math.round(balance*100)/100;
+  document.getElementById("value").innerText = Math.round(balance * 100) / 100;
 
 });
 
-document.querySelector("form").addEventListener("submit", async function(event) {
+document.querySelector("form").addEventListener("submit", async function (event) {
   event.preventDefault(); //prevent the default action of the form to clear the form
 
   const button = event.target.querySelector("#submit-btn");
@@ -19,14 +19,14 @@ document.querySelector("form").addEventListener("submit", async function(event) 
   const deposit = parseFloat(document.getElementById("deposit-amount").value);
   const withdraw = parseFloat(document.getElementById("withdrawal-amount").value);
 
-  if(deposit.length != 0 && deposit > 0){
+  if (deposit.length != 0 && deposit > 0) {
     await decentralbank.deposit(deposit);
     document.getElementById("deposit-amount").value = "";
   } else {
 
   };
 
-  if(withdraw.length != 0 && withdraw > 0){
+  if (withdraw.length != 0 && withdraw > 0) {
     await decentralbank.withdraw(withdraw);
     document.getElementById("withdrawal-amount").value = "";
   } else {
@@ -34,7 +34,7 @@ document.querySelector("form").addEventListener("submit", async function(event) 
   };
 
   const balance = await decentralbank.checkbalance();
-  document.getElementById("value").innerText = Math.round(balance*100)/100;
+  document.getElementById("value").innerText = Math.round(balance * 100) / 100;
 
   button.removeAttribute("disabled"); //enable the button after the form is submitted
 
