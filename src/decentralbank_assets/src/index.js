2bf4130c8f7102e5292a538abdadd892b635f2f4
Aniket <aniket.kini1@gmail.com>
2024-06-03 11:43:02 +0000

    commit

--- src/decentralbank_assets/src/index.js
+++ src/decentralbank_assets/src/index.js
@@ -1,19 +1,7 @@
-import { decentralbank } from "../../declarations/decentralbank";
+import {
+  debank
+} from "../../declarations/decentralbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await decentralbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener('load', function () {  //will get executed after the page is loaded
+  console.log("Page loaded");
 });
